803a086eb55a7ed6e89720aa42cd448f93d3e6c8
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2021-05-17 21:54:03 -0400

    prod build working

--- app/webpack.prod.js
+++ app/webpack.prod.js
@@ -3,16 +3,9 @@ const webpack = require("webpack");
 const commonConfig = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = merge(commonConfig, {
   mode: "production",
-  // output: {
-  // Can't have content hash into bundle names because we load the bundles dynamically contrary
-  // to having the htmlwebpackplugin automatically putting the bundles in the index.html file
-  // filename: "[name].[contenthash].bundle.js",
-  // chunkFilename: "[name].[contenthash].bundle.js",
-  //   path: path.resolve(__dirname, "../NectariComponents/dist"),
-  //   publicPath: "/dist/"
-  // },
   devtool: "cheap-module-source-map",
   optimization: {
     minimize: true,
