7d60bcad55d0673af925079ddfe54c43f33e0854
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2021-05-17 20:58:36 -0400

    dev reload env working

--- app/webpack.dev.js
+++ app/webpack.dev.js
@@ -4,14 +4,16 @@ const { merge } = require("webpack-merge");
 
 module.exports = merge(commonConfig, {
   mode: "development",
+  target: "web",
   devtool: "eval-source-map",
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    publicPath: "/",
+    contentBase: "./dist",
     compress: true,
     port: 3000,
+    watchContentBase: true,
+    hot: true,
     proxy: {
-      context: () => true,
+      context: ["/api"],
       target: "http://localhost:3080",
     },
   },
